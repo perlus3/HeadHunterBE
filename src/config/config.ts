5e0c77af94d61d430a9711685de8a045914a8dde
Artur Ponieczynski <ponieczynski@gmail.com>
2023-04-23 01:16:25 +0200

    Bug fixes.

--- src/config/config.ts
+++
@@ -1,7 +0,0 @@
-export const configuration = {
-  userNameSMTP: 'example@mail.pl',
-  userPasswordSMTP: 'examplePassword',
-  hostSMTP: 'localhost',
-  portSMTP: 2500,
-  emailSendFromSMTP: 'admin@headhunter.pl',
-};

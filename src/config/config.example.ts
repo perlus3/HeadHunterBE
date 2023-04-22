fce8f081156ea3cb1f4f7f2ddd3188083d401e99
Artur Ponieczynski <ponieczynski@gmail.com>
2023-04-23 01:18:47 +0200

    Updated .gitignore, port change in main.ts

---
+++ src/config/config.example.ts
@@ -0,0 +1,7 @@
+export const configuration = {
+  userNameSMTP: 'example@mail.pl',
+  userPasswordSMTP: 'password',
+  hostSMTP: 'host',
+  portSMTP: '',
+  emailSendFromSMTP: 'example@mail.pl',
+};

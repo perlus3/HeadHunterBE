135b86fac7e1de39a317b6906cbbfeb1dfda0bfc
RadoslawBaran <baradoslaw@gmail.com>
2023-05-02 17:42:04 +0200

    Add getStudentData method to users service. It can return data for now, needs data processing and validation. Make bonusProjectUrls field nullable again.

--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -35,4 +35,54 @@ export class UsersService {
       };
     }) as AvailableStudentData[];
   }
+
+  async getStudentData(id: string): Promise<OneStudentData> {
+    const student = await StudentEntity.findOne({
+      select: [
+        'id',
+        'bio',
+        'githubUsername',
+        'courseCompletion',
+        'courseEngagement',
+        'projectDegree',
+        'teamProjectDegree',
+        'portfolioUrls',
+        'bonusProjectUrls',
+        'expectedTypeWork',
+        'targetWorkCity',
+        'expectedContractType',
+        'expectedSalary',
+        'canTakeApprenticeship',
+        'monthsOfCommercialExp',
+        'education',
+        'workExperience',
+      ],
+      relations: ['user'],
+      where: {
+        id,
+      },
+    });
+
+    return {
+      firstName: student.user.firstName,
+      lastName: student.user.lastName,
+      bio: student.bio,
+      githubUsername: student.githubUsername,
+      courseCompletion: student.courseCompletion,
+      courseEngagement: student.courseEngagement,
+      projectDegree: student.projectDegree,
+      teamProjectDegree: student.teamProjectDegree,
+      portfolio: student.portfolioUrls,
+      teamProjectUrls: null,
+      bonusProjectUrls: student.bonusProjectUrls,
+      expectedTypeWork: student.expectedTypeWork,
+      targetWorkCity: student.targetWorkCity,
+      expectedContractType: student.expectedContractType,
+      expectedSalary: student.expectedSalary,
+      canTakeApprenticeship: student.canTakeApprenticeship,
+      monthsOfCommercialExp: student.monthsOfCommercialExp,
+      education: student.education,
+      workExperience: student.workExperience,
+    } as OneStudentData;
+  }
 }

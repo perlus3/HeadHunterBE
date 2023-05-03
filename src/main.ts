31d78ae5d37acb7305fa6521f531f098ddf21bd8
Kamil <perlegus@gmail.com>
2023-05-03 12:48:58 +0200

    ustawione corsy dla FE

--- src/main.ts
+++ src/main.ts
@@ -7,6 +7,11 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.use(cookieParser());
 
+  app.enableCors({
+    origin: 'http://localhost:3001/',
+    // jak nie będzie działać to poprawcie ten adres albo dajcie '*' zamiast 'http://localhost:3001/' ,
+  });
+
   app.useGlobalPipes(
     new ValidationPipe({
       transform: true,
